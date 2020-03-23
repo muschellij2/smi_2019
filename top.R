7410e41977653204ef3c5f2b70619418c54ac4e0
muschellij2 <muschellij2@gmail.com>
2020-03-23 16:23:02 -0400

    added top.R

---
+++ top.R
@@ -0,0 +1,27 @@
+top = function(x, n) {
+  xx = x[1:n, 1:n]
+  hist(xx)
+  print(xx)
+}
+
+
+#' @param ... pass additional methods to [graphics::hist]
+top = function(x, n, ...) {
+  xx = x[1:n, 1:n]
+  hist(xx, ...)
+  print(xx)
+}
+
+
+run_top_stat = function(x, n = 4, stat = c("mean", "median", "sd"),  ...) {
+  stat = match.arg(stat)
+  print(stat)
+  if (!assertthat::is.number(n)) { # make sure n is just one value and numeric
+    stop("n should be a number of length 1!")
+  }
+  top_x = top(x, n = n, ...)
+  func = switch(stat, mean = function(x) mean(x, na.rm = TRUE),
+                "median" = median, "sd" = sd)
+  return(func(top_x))
+  stop("this will never be executed!")
+}
